0bc02b037828a48209852e4440ebf1b929113d60
Mykola Gumeniuk <gumeniuk.mykola@gmail.com>
2026-04-18 22:41:23 +0300

    fix: add profile edit page

---
+++ app/(private routes)/profile/edit/page.tsx
@@ -0,0 +1,8 @@
+export default function EditProfilePage() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Edit Profile</h1>
+      <p>This is edit profile page</p>
+    </div>
+  );
+}
