46d3a71fb120d5feaf8e4f306a41016fe5baf409
Mykola Gumeniuk <gumeniuk.mykola@gmail.com>
2026-04-18 21:27:41 +0300

    fix: update fetchNotes in prefetch

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -23,7 +23,12 @@ export default async function Page({ params }: Props) {
 
   await queryClient.prefetchQuery({
     queryKey: ["notes", 1, normalizedTag, ""],
-    queryFn: () => fetchNotes(1, normalizedTag, ""),
+    queryFn: () =>
+  fetchNotes({
+    page: 1,
+    tag: normalizedTag,
+    search: "",
+  }),
   });
 
   return (
